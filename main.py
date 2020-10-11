95013f5f3e419f53df4d8733137586bc7cd725a3
aliprf <aliprf@gmail.com>
2020-10-10 21:30:14 -0600

    normalized And Equalized Histrogram developed

--- main.py
+++ main.py
@@ -18,6 +18,33 @@ def load_images():
     return imgs
 
 
+def print_img(img, histo_new, histo_old, index, L):
+    dpi = 80
+    width = img.shape[0]
+    height = img.shape[1]
+    if height > width:
+        figsize = (img.shape[0]*4) / float(dpi), (height)/ float(dpi)
+        fig, axs = plt.subplots(1, 3, gridspec_kw={'width_ratios': [3, 1,1]}, figsize=figsize)
+    else:
+        figsize = (width) / float(dpi), (height*4) / float(dpi)
+        fig, axs = plt.subplots(3, 1, gridspec_kw={'height_ratios': [3, 1,1]}, figsize=figsize)
+
+    fig.suptitle("Enhanced Image with L:" + str(L))
+    axs[0].title.set_text("Enhanced Image")
+    axs[0].imshow(img, vmin=np.amin(img), vmax=np.amax(img), cmap='gray')
+
+    axs[1].title.set_text("Equalized histogram")
+    axs[1].plot(histo_new, color='#f77f00')
+    axs[1].bar(np.arange(len(histo_new)), histo_new, color='#003049')
+
+    axs[2].title.set_text("Main histogram")
+    axs[2].plot(histo_old, color='#ef476f')
+    axs[2].bar(np.arange(len(histo_old)), histo_old, color='#b7b7a4')
+    plt.tight_layout()
+    plt.savefig("e" + index + str(L)+".pdf")
+    plt.savefig("e" + index + str(L)+".png")
+
+
 def print_histogram(_histrogram, name, title):
     plt.figure()
     plt.title(title)
@@ -44,25 +71,37 @@ def generate_histogram(img, print, index):
     gr_hist /= (gr_img.shape[0] * gr_img.shape[1])
     if print:
         print_histogram(gr_hist, name="eq_"+str(index), title="Normalized Histogram")
-    return gr_hist
+    return gr_hist, gr_img
 
 
-def equalize_histogram(histo, L):
+def equalize_histogram(img, histo, L):
     eq_histo = np.zeros_like(histo)
+    en_img = np.zeros_like(img)
     for i in range(len(histo)):
         eq_histo[i] = int((L - 1) * np.sum(histo[0:i]))
     print_histogram(eq_histo, name="eq_"+str(index), title="Equalized Histogram")
+    '''enhance image as well:'''
+    for x_pixel in range(img.shape[0]):
+        for y_pixel in range(img.shape[1]):
+            pixel_val = int(img[x_pixel, y_pixel])
+            en_img[x_pixel, y_pixel] = eq_histo[pixel_val]
+    '''creating new histogram'''
+    hist_img, _ = generate_histogram(en_img, print=False, index=index)
+    print_img(img=en_img, histo_new=hist_img, histo_old=histo, index=str(index), L=L)
 
 
 if __name__ == '__main__':
+    L=50
     print("\r\nLoading Images:")
     imgs = load_images()
     print("\r\ngenerating HistogramS:")
+    gr_img_arr = []
     grayscale_histogram_arr = []
     eq_histogram_arr = []
     index = 0
     for img in tqdm(imgs):
-        hist_img = generate_histogram(img, print=True, index=index)
+        hist_img, gr_img = generate_histogram(img, print=True, index=index)
         grayscale_histogram_arr.append(hist_img)
-        eq_histogram_arr.append(equalize_histogram(hist_img, 8))
+        gr_img_arr.append(gr_img)
+        eq_histogram_arr.append(equalize_histogram(gr_img, hist_img, L))
         index += 1
